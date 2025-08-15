cf6be7dcde4e892c869b4867b5a91afce4b48b60
laura2ndrea <laura2ndrea12@gmail.com>
2025-08-15 06:51:32 -0500

    feat: creación de clase personalizada para códigos 404

---
+++ src/main/java/com/laura/payment_links/exception/PaymentLinkNotFoundException.java
@@ -0,0 +1,10 @@
+package com.laura.payment_links.exception;
+
+import java.util.UUID;
+
+// Clase personalizada para el código 404 NOT_FOUND
+public class PaymentLinkNotFoundException extends RuntimeException {
+    public PaymentLinkNotFoundException(UUID id) {
+        super("PaymentLink con ID " + id + " no encontrado");
+    }
+}
